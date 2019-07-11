543462c3e5c0ccfe3052e07515cf00d5d876ee16
Jason Franco Quintero <jada183.jf@gmail.com>
2019-07-11 14:39:37 +0200

    Listado de comentarios con filtros

--- src/main/java/com/outplaying/repository/ICommentRepository.java
+++ src/main/java/com/outplaying/repository/ICommentRepository.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.outplaying.model.Comment;
 import com.outplaying.model.Post;
+import com.outplaying.model.User;
 
 
 public interface ICommentRepository extends JpaRepository<Comment, Long> {
@@ -16,4 +17,10 @@ public interface ICommentRepository extends JpaRepository<Comment, Long> {
 	@Transactional
 	@Query("SELECT c  FROM Comment c WHERE c.post = ?1")
 	public List<Comment> commentsByPost(Post post);
+	
+	@Transactional
+	@Query("SELECT c  FROM Comment c WHERE c.user = ?1")
+	public List<Comment> commentsByIdUser(User user);
 }
+
+
--- src/main/java/com/outplaying/service/implementation/ComentServiceImpl.java
+++ src/main/java/com/outplaying/service/implementation/ComentServiceImpl.java
@@ -13,8 +13,10 @@ import org.springframework.stereotype.Service;
 import com.outplaying.dto.CommentDTO;
 import com.outplaying.model.Comment;
 import com.outplaying.model.Post;
+import com.outplaying.model.User;
 import com.outplaying.repository.ICommentRepository;
 import com.outplaying.repository.IPostRepository;
+import com.outplaying.repository.IUserRepository;
 import com.outplaying.service.ICommentService;
 @Service
 public class ComentServiceImpl implements ICommentService {
@@ -26,6 +28,9 @@ public class ComentServiceImpl implements ICommentService {
 	
 	@Autowired
 	private IPostRepository postRepository;
+	
+	@Autowired
+	private IUserRepository userRepository;
 	
 	@Override
 	public List<CommentDTO> getListByPostId(Long idPost) {
@@ -46,14 +51,24 @@ public class ComentServiceImpl implements ICommentService {
 
 	@Override
 	public List<CommentDTO> getListByUserId(Long idUser) {
-		// TODO Auto-generated method stub
-		return null;
+		List<CommentDTO> commentsDTO = new ArrayList<>();
+		List<Comment> comentsBack = new ArrayList<>();
+		Optional<User> userOp = userRepository.findById(idUser);
+		if(userOp.isPresent()) { 
+			comentsBack = commentRepository.commentsByIdUser(userOp.get());
+			for (Comment c : comentsBack) {
+				commentsDTO.add(modelMapper.map(c, CommentDTO.class));
+			}
+			return commentsDTO;
+		} else {
+			throw new EntityNotFoundException("user  with id " + idUser + " does not exists");
+		}
 	}
 	
 	@Override
 	public CommentDTO getCommentById(Long commentId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return modelMapper.map(commentRepository.getOne(commentId), CommentDTO.class);
 	}
 
 	@Override
